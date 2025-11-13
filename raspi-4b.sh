6e3dcb99b2307f66e1100a5d1b00ad9660670d39
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-11-13 11:29:57 +0900

    ttt

--- raspi-4b.sh
+++ raspi-4b.sh
@@ -1,14 +1,17 @@
-======================================================================
-Client (Aircraft)
-======================================================================
+# ======================================================================
+# Client (Aircraft)
+# ======================================================================
 
+# wpa_supplicantは競合避けるため使用しません
 sudo systemctl stop wpa_supplicant.service
 sudo systemctl mask wpa_supplicant.service
 
+# WifiデバイスをNetworkManagerの管理下から除外します
 echo "[keyfile]
 unmanaged-devices=interface-name:wlan*;interface-name:p2p-*" | sudo tee /etc/NetworkManager/conf.d/unmanaged.conf
 sudo systemctl restart NetworkManager
 
+# Aircraft用wpa_supplicantの設定
 echo 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 p2p_go_intent=0
@@ -16,25 +19,29 @@ device_name=Aircraft
 device_type=6-0050F204-1
 config_methods=virtual_push_button physical_display keypad' | sudo tee /etc/wpa_supplicant/wpa_supplicant-aircraft.conf
 
+# 使用するWifiデバイスのwpa_supplicantサービスを追加
 sudo systemctl enable wpa_supplicant@wlan0.service
+# Aircraft用の設定を読み込むようにoverride
 sudo systemctl edit wpa_supplicant@wlan0.service
 ```
 [Service]
 ExecStart=
 ExecStart=/usr/sbin/wpa_supplicant -Dnl80211 -iwlan0 -c/etc/wpa_supplicant/wpa_supplicant-aircraft.conf
 ```
-
+# 使用するWifiデバイスのwpa_supplicantサービスを起動
 sudo systemctl daemon-reload
 sudo systemctl enable --now wpa_supplicant@wlan0.service
 
-======================================================================
+# ======================================================================
 
+# 関連サービスの状態を確認
 sudo systemctl status NetworkManager
 sudo systemctl status wpa_supplicant.service
 sudo systemctl status wpa_supplicant@wlan0.service
 
-======================================================================
+# ======================================================================
 
+# 既存の設定を初期化
 sudo wpa_cli -i wlan0 p2p_cancel
 sudo wpa_cli -i wlan0 p2p_stop_find
 sudo wpa_cli -i wlan0 p2p_flush
@@ -43,18 +50,19 @@ sudo ip link set wlan0 down
 sudo ip link set wlan0 up
 sudo systemctl restart wpa_supplicant@wlan0
 
-======================================================================
+# ======================================================================
 
+# GO(group owner)に接続
 sudo wpa_cli -i wlan0 p2p_find
 sudo wpa_cli -i wlan0 p2p_peers
-
+# 検出されたGOのMACアドレスに対してJoin
 sudo wpa_cli -i wlan0 p2p_connect bc:09:1b:1d:15:92 pbc join
 
-======================================================================
+# ======================================================================
 
 <GO wps_pbc>
 
-======================================================================
+# ======================================================================
 
 iw dev $(basename /sys/class/net/p2p-wlan0-*) info 2>/dev/null
 sudo wpa_cli -i "$(basename /sys/class/net/p2p-wlan0-*)" status 2>/dev/null
@@ -63,10 +71,10 @@ ip addr show $(ls /sys/class/net/ | grep ^p2p-wlan0-)
 iw dev $(basename /sys/class/net/p2p-wlan0-*) station dump
 sudo wpa_cli -i wlan0 p2p_peer bc:09:1b:1d:15:92
 
-======================================================================
+# ======================================================================
 
 sudo ip addr add 192.168.49.2/24 dev p2p-wlan0-0
 
-======================================================================
+# ======================================================================
 
 sudo journalctl -u wpa_supplicant@wlan0 -f

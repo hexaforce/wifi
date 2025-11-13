6e63447cfb4971b46f421320f2613a7ac3a63614
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-11-13 11:50:25 +0900

    xxxx

--- raspi-4b.sh
+++ raspi-4b.sh
@@ -9,19 +9,20 @@ sudo systemctl mask wpa_supplicant.service
 # WifiデバイスをNetworkManagerの管理下から除外します
 echo "[keyfile]
 unmanaged-devices=interface-name:wlan*;interface-name:p2p-*" | sudo tee /etc/NetworkManager/conf.d/unmanaged.conf
+
 sudo systemctl restart NetworkManager
 
-# Aircraft用wpa_supplicantの設定
+# ドローン用のwpa_supplicant設定を作成
 echo 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 p2p_go_intent=0
 device_name=Aircraft
-device_type=6-0050F204-1
+device_type=1-0050F204-1
 config_methods=virtual_push_button physical_display keypad' | sudo tee /etc/wpa_supplicant/wpa_supplicant-aircraft.conf
 
 # 使用するWifiデバイスのwpa_supplicantサービスを追加
 sudo systemctl enable wpa_supplicant@wlan0.service
-# Aircraft用の設定を読み込むようにoverride
+# ドローン用のwpa_supplicant設定を読み込むようにoverride
 sudo systemctl edit wpa_supplicant@wlan0.service
 ```
 [Service]
@@ -55,17 +56,17 @@ sudo systemctl restart wpa_supplicant@wlan0
 # GO(group owner)に接続開始
 sudo wpa_cli -i wlan0 p2p_find
 sudo wpa_cli -i wlan0 p2p_peers
-# 検出されたGOのMACアドレスに対してJoin
+# 検出されたGOのMACアドレスに対してPBC(WPS Push Button Configuration)要求する
 sudo wpa_cli -i wlan0 p2p_connect bc:09:1b:1d:15:92 pbc join
 
 # ======================================================================
 
 # <GO wps_pbc>
-# GO側でPBC(WPS Push Button Configuration)要求を受け入れる
+# GO側でPBC(WPS Push Button Configuration)要求の受け入れ待ち
 
 # ======================================================================
 
-# 接続後のステータス確認
+# 接続確立後のステータス確認
 iw dev $(basename /sys/class/net/p2p-wlan0-*) info 2>/dev/null
 sudo wpa_cli -i "$(basename /sys/class/net/p2p-wlan0-*)" status 2>/dev/null
 ip addr show $(ls /sys/class/net/ | grep ^p2p-wlan0-)
--- ubuntu24.sh
+++ ubuntu24.sh
@@ -1,40 +1,49 @@
 
-======================================================================
-GO (Ground Station)
-======================================================================
+# ======================================================================
+# GO (Ground Station)
+# ======================================================================
 
+# wpa_supplicantは競合避けるため使用しません
 sudo systemctl stop wpa_supplicant.service
 sudo systemctl mask wpa_supplicant.service
 
+# WifiデバイスをNetworkManagerの管理下から除外します
 echo "[keyfile]
 unmanaged-devices=sinterface-name:wlo*;interface-name:p2p-*" | sudo tee /etc/NetworkManager/conf.d/unmanaged.conf
+
 sudo systemctl restart NetworkManager
 
+# 地上局用のwpa_supplicant設定を作成
 echo "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 device_name=GroundStation
-device_type=1-0050F204-1
+device_type=6-0050F204-1
 p2p_ssid_postfix=_FPV
 config_methods=virtual_push_button physical_display keypad" | sudo tee /etc/wpa_supplicant/wpa_supplicant-ground.conf
 
+# 使用するWifiデバイスのwpa_supplicantサービスを追加
+sudo systemctl enable wpa_supplicant@wlo1.service
+# 地上局用のwpa_supplicant設定を読み込むようにoverride
 sudo systemctl edit wpa_supplicant@wlo1.service
 ```
 [Service]
 ExecStart=
 ExecStart=/usr/sbin/wpa_supplicant -Dnl80211 -iwlo1 -c/etc/wpa_supplicant/wpa_supplicant-ground.conf
 ```
-
+# 使用するWifiデバイスのwpa_supplicantサービスを起動
 sudo systemctl daemon-reload
 sudo systemctl enable --now wpa_supplicant@wlo1.service
 
-======================================================================
+# ======================================================================
 
+# 関連サービスの状態を確認
 sudo systemctl status NetworkManager
 sudo systemctl status wpa_supplicant.service
 sudo systemctl status wpa_supplicant@wlo1.service
 
-======================================================================
+# ======================================================================
 
+# 既存の設定を初期化
 sudo wpa_cli -i wlo1 p2p_cancel
 sudo wpa_cli -i wlo1 p2p_stop_find
 sudo wpa_cli -i wlo1 p2p_flush
@@ -43,31 +52,37 @@ sudo ip link set wlo1 down
 sudo ip link set wlo1 up
 sudo systemctl restart wpa_supplicant@wlo1
 
-======================================================================
+# ======================================================================
 
+# グループを新規作成し、仮想アクセスポイントを起動
 sudo wpa_cli -i wlo1 p2p_group_add freq=2437
 
+# ======================================================================
+
+# <Client join>
+# ClientからのPBC(WPS Push Button Configuration)要求を待つ
+
+# ======================================================================
+
+# PBC(WPS Push Button Configuration)要求の受理
+sudo wpa_cli -p /var/run/wpa_supplicant -i p2p-wlo1-0 wps_pbc
+
+# ======================================================================
+
+# 接続確立後のステータス確認
 iw dev $(basename /sys/class/net/p2p-wlo1-*) info 2>/dev/null
 sudo wpa_cli -i "$(basename /sys/class/net/p2p-wlo1-*)" status 2>/dev/null
 ip addr show $(ls /sys/class/net/ | grep ^p2p-wlo1-)
 
-======================================================================
-
-<Client join>
-
-======================================================================
-
-sudo wpa_cli -p /var/run/wpa_supplicant -i p2p-wlo1-0 wps_pbc
-
-======================================================================
-
 iw dev $(basename /sys/class/net/p2p-wlo1-*) station dump
 sudo wpa_cli -i wlo1 p2p_peer da:3a:dd:09:24:2a
 
-======================================================================
+# ======================================================================
 
+# IPv4アドレスを割り当て
 sudo ip addr add 192.168.49.1/24 dev p2p-wlo1-0
 
-======================================================================
+# ======================================================================
 
+# サービスログ確認
 sudo journalctl -u wpa_supplicant@wlo1 -f

e756ab7d960fdbdfd8e4e36550f69d19e8295530
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-11-15 10:58:14 +0900

    test

--- aircraft-example.sh
+++ aircraft-example.sh
@@ -1,24 +1,35 @@
 # ======================================================================
-# Client (Aircraft)
+# 基本設定
 # ======================================================================
 
-# wpa_supplicantは競合避けるため使用しません
+# wpa_supplicantは競合避けるためmask
 sudo systemctl stop wpa_supplicant.service
 sudo systemctl mask wpa_supplicant.service
 
-# WifiデバイスをNetworkManagerの管理下から除外します
-echo "[keyfile]
-unmanaged-devices=interface-name:wlan*;interface-name:p2p-*" | sudo tee /etc/NetworkManager/conf.d/unmanaged.conf
+# WifiデバイスをNetworkManagerの管理下から除外
+sudo tee /etc/NetworkManager/conf.d/unmanaged.conf >/dev/null <<'EOF'
+[keyfile]
+unmanaged-devices=interface-name:wlan*;interface-name:p2p-*
+EOF
 
+# NetworkManagerを再起動しwifiが管理されていない事を確認
 sudo systemctl restart NetworkManager
+nmcli device status
+
+
+# ======================================================================
+# P2P-Client mode
+# ======================================================================
 
 # ドローン用のwpa_supplicant設定を作成
-echo 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+sudo tee /etc/wpa_supplicant/wpa_supplicant-aircraft.conf >/dev/null <<'EOF'
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 p2p_go_intent=0
 device_name=Aircraft
 device_type=1-0050F204-1
-config_methods=virtual_push_button physical_display keypad' | sudo tee /etc/wpa_supplicant/wpa_supplicant-aircraft.conf
+config_methods=virtual_push_button physical_display keypad
+EOF
 
 # 使用するWifiデバイスのwpa_supplicantサービスを追加
 sudo systemctl enable wpa_supplicant@wlan0.service
@@ -56,13 +67,13 @@ sudo systemctl restart wpa_supplicant@wlan0
 # GO(group owner)に接続開始
 sudo wpa_cli -i wlan0 p2p_find
 sudo wpa_cli -i wlan0 p2p_peers
-# 検出されたGOのMACアドレスに対してPBC(WPS Push Button Configuration)要求する
+# 検出されたGOのMACアドレスに対してPBCを要求する
 sudo wpa_cli -i wlan0 p2p_connect bc:09:1b:1d:15:92 pbc join
 
 # ======================================================================
 
 # <GO wps_pbc>
-# GO側でPBC(WPS Push Button Configuration)要求の受け入れ待ち
+# GO側でPBC要求の受け入れ待ち
 
 # ======================================================================
 
@@ -83,3 +94,33 @@ ip addr show $(ls /sys/class/net/ | grep ^p2p-wlan0-)
 
 iw dev $(basename /sys/class/net/p2p-wlan0-*) station dump
 sudo wpa_cli -i wlan0 p2p_peer bc:09:1b:1d:15:92
+
+
+# ======================================================================
+# managed mode
+# ======================================================================
+
+# 1. wpa_supplicant設定
+sudo tee /etc/wpa_supplicant/wpa_supplicant-wlan2.conf >/dev/null <<'EOF'
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+EOF
+# SSID/Pass追記
+wpa_passphrase "AP-GroundStation" "wlxe0e1a91d6625" | sudo tee -a /etc/wpa_supplicant/wpa_supplicant-wlan2.conf
+
+# サービスを起動
+sudo systemctl enable wpa_supplicant@wlan2
+# IP
+sudo systemctl edit wpa_supplicant@wlan2
+```
+[Service]
+ExecStartPost=/bin/sleep 2
+ExecStartPost=/usr/sbin/ip addr add 192.168.50.2/24 dev wlan2
+```
+
+ip addr show wlan2
+
+wpa_cli -i wlan2 status
+wpa_cli -i wlan2 signal_poll
+
+sudo journalctl -u wpa_supplicant@wlan2 -f
--- ground-station-example.sh
+++ ground-station-example.sh
@@ -1,17 +1,27 @@
-
 # ======================================================================
-# GO (Ground Station)
+# 基本設定
 # ======================================================================
 
-# wpa_supplicantは競合避けるため使用しません
+# 競合避けるためmask
 sudo systemctl stop wpa_supplicant.service
 sudo systemctl mask wpa_supplicant.service
 
-# WifiデバイスをNetworkManagerの管理下から除外します
-echo "[keyfile]
-unmanaged-devices=sinterface-name:wlo*;interface-name:p2p-*" | sudo tee /etc/NetworkManager/conf.d/unmanaged.conf
+sudo systemctl stop hostapd-network.service
+sudo systemctl mask hostapd-network.service
 
+# WifiデバイスをNetworkManagerの管理下から除外
+sudo tee /etc/NetworkManager/conf.d/unmanaged.conf >/dev/null <<'EOF'
+[keyfile]
+unmanaged-devices=sinterface-name:wlo*;interface-name:wlx*;interface-name:p2p-*
+EOF
+
+# NetworkManagerを再起動しwifiが管理されていない事を確認
 sudo systemctl restart NetworkManager
+nmcli device status
+
+# ======================================================================
+# P2P-GO mode
+# ======================================================================
 
 # 地上局用のwpa_supplicant設定を作成
 echo "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
@@ -60,7 +70,7 @@ sudo wpa_cli -i wlo1 p2p_group_add freq=2437
 # ======================================================================
 
 # <Client join>
-# ClientからのPBC(WPS Push Button Configuration)要求を待つ
+# ClientからのPBC要求を待つ
 
 # ======================================================================
 
@@ -86,3 +96,74 @@ ip addr show $(ls /sys/class/net/ | grep ^p2p-wlo1-)
 
 iw dev $(basename /sys/class/net/p2p-wlo1-*) station dump
 sudo wpa_cli -i wlo1 p2p_peer da:3a:dd:09:24:2a
+
+# ======================================================================
+# AP mode
+# ======================================================================
+
+# 1. hostapd.service (AP起動)
+sudo systemctl unmask hostapd.service 2>/dev/null || true
+
+sudo tee /etc/hostapd/hostapd.conf >/dev/null <<'EOF'
+interface=wlxe0e1a91d6625
+driver=nl80211
+ssid=AP-GroundStation
+hw_mode=g
+channel=6
+wmm_enabled=0
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=2
+wpa_passphrase=wlxe0e1a91d6625
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=TKIP
+rsn_pairwise=CCMP
+EOF
+
+# hostapd設定の参照パスを更新
+sudo sed -i 's/^#DAEMON_CONF=.*/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' /etc/default/hostapd
+
+# IPの割り当てとhostapdがdnsmasqより先に起動するように設定
+sudo systemctl edit hostapd
+```
+[Service]
+ExecStartPost=/bin/sleep 2
+ExecStartPost=/usr/sbin/ip addr add 192.168.50.1/24 dev wlxe0e1a91d6625
+
+[Unit]
+Before=dnsmasq.service
+```
+
+# 2. dnsmasq.service (DHCP起動)
+sudo systemctl unmask dnsmasq.service 2>/dev/null || true
+
+# port=0 DNSは提供しない（systemd-resolvedに任せる）
+sudo tee /etc/dnsmasq.conf >/dev/null <<'EOF'
+interface=wlxe0e1a91d6625
+bind-interfaces
+dhcp-range=192.168.50.10,192.168.50.50,255.255.255.0,24h
+port=0
+EOF
+
+# 更新して有効化
+sudo systemctl daemon-reload
+
+sudo systemctl enable hostapd
+sudo systemctl enable dnsmasq
+
+# 再起動して確認
+sudo systemctl restart hostapd
+sudo systemctl restart dnsmasq
+
+sudo systemctl status hostapd
+sudo systemctl status dnsmasq
+
+ip addr show wlxe0e1a91d6625
+iw dev wlxe0e1a91d6625 info
+
+# ログ
+sudo journalctl -u hostapd-network -f
+sudo journalctl -u hostapd -f
+sudo journalctl -u dnsmasq -f
+sudo journalctl -xeu dnsmasq.service
